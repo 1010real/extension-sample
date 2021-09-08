9068301800e05f566a6570344a3382b0678b70a6
1010real <moka0329@gmail.com>
2021-09-08 22:56:42 +0900

    use active tab sample

--- background.js
+++ background.js
@@ -1,9 +1,20 @@
+const storageKey = '__sample_color'
+
 chrome.runtime.onInstalled.addListener(({}) => {
-  console.log('oninstalled')
-  chrome.storage.sync.set({ ['__sample_color']: '#ffff00' })
+  chrome.storage.sync.set({ [storageKey]: '#888888' })
 })
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  console.log('tabs.onupdated')
   chrome.tabs.sendMessage(tabId, 'SET_BG')
+})
+
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+  if (request !== 'INITIALIZE') return
+  const queryOptions = { active: true, currentWindow: true }
+  const [tab] = await chrome.tabs.query(queryOptions)
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id, allFrames: true },
+    files: ['content-script.js'],
+  })
+  chrome.tabs.sendMessage(tab.id, 'SET_BG')
 })
